27d51a1e9f88a1e6bdc26cfd39bd1fff8b548c87
Luis Chavez <luichav3z@gmail.com>
2022-01-26 17:20:25 -0800

    finsihed lab

---
+++ main_app/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import redirect, render
+from .models import Bird, Toy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .forms import FeedingForm
+# Create your views here.
+
+def assoc_toy(request, bird_id, toy_id):
+    Bird.objects.get(id=bird_id).toys.add(toy_id)
+    return redirect('detail', bird_id=bird_id)
+
+class BirdCreate(CreateView):
+    model = Bird
+    fields = ['name', 'breed', 'description', 'age']
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = ['breed', 'description', 'age']
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds/'
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def birds_index(request):
+    birds = Bird.objects.all()
+    return render(request, 'birds/index.html', { 'birds': birds })
+
+def birds_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  feeding_form = FeedingForm()
+  toys_bird_doesnt_have = Toy.objects.exclude(id__in= bird.toys.all().values_list('id'))
+  return render(request, 'birds/detail.html', { 
+      'bird': bird,
+      'feeding_form': feeding_form,
+      'toys': toys_bird_doesnt_have
+       })
+
+def add_feeding(request, bird_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
